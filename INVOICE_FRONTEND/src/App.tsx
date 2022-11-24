b38181e64309ae54a8cfc973ab6a442978863972
tanyasismayiana <tanyamayiana@gmail.com>
2022-11-24 09:39:46 +0300

    Added fronted folder

---
+++ INVOICE_FRONTEND/src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Signup from "./components/Signup"
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Header/>
+      <Signup/>
+    
+    </div>
+  )
+}
+
+export default App
